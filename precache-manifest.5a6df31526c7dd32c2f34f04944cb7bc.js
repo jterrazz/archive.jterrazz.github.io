dc36781622040399679765221302526873ba0b99
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-12-02 02:39:34 +0100

    Updates

---
+++ precache-manifest.5a6df31526c7dd32c2f34f04944cb7bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071c1172e312f1b22bc57fb4b3809e1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1235972f6932aeb746ba",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5f44679e0001370f194b",
+    "url": "/static/js/2.4cbb4a36.chunk.js"
+  },
+  {
+    "revision": "1235972f6932aeb746ba",
+    "url": "/static/js/main.47b97a9d.chunk.js"
+  },
+  {
+    "revision": "9ba548e7324b12e8ae05",
+    "url": "/static/js/runtime-main.0ec78e2e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
