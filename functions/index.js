76d937504543de9e904393e3e79a9144a5a7d78f
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-12-08 07:45:36 +0100

    Firebase contact form function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const RECEIVER_EMAIL = "jterrazz@protonmail.com";
+
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+const { email, password } = functions.config().gmail;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+exports.submit = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (req.method !== 'POST') {
+      return;
+    }
+
+    const mailOptions = {
+      replyTo: req.body.email,
+      to: RECEIVER_EMAIL,
+      subject: `From my website - ${req.body.fullName} - ${req.body.email}`,
+      text: req.body.message,
+      html: `<p>${req.body.message}`
+    };
+
+    mailTransport.sendMail(mailOptions);
+
+    res.status(200).send();
+  });
+});
